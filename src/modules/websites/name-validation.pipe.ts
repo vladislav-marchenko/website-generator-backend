aab121f9b9af208a171371fbff6d41b6df601265
Владислав <vladislav@MacBook-Air-Vladislav.local>
2025-01-31 20:06:09 +0300

    changed website name pattern

--- src/modules/websites/name-validation.pipe.ts
+++ src/modules/websites/name-validation.pipe.ts
@@ -2,14 +2,14 @@ import { PipeTransform, Injectable, BadRequestException } from '@nestjs/common'
 
 @Injectable()
 export class NameValidationPipe implements PipeTransform {
-  private readonly namePattern = /^(?!-)[A-Za-z0-9-]{1,63}(?<!-)$/
+  private readonly namePattern = /^(?!-)[A-Za-z0-9-]{2,20}(?<!-)$/
 
   transform(value: string) {
     const isValid = this.namePattern.test(value)
 
     if (!isValid) {
       throw new BadRequestException(
-        'The name can be 1-63 characters long and contain letters, numbers, or hyphens (except at the beginning and end). Spaces and special characters are not allowed.',
+        'The name can be 2-20 characters long and contain letters, numbers, or hyphens (except at the beginning and end). Spaces and special characters are not allowed.',
       )
     }
 
--- src/modules/websites/websites.dto.ts
+++ src/modules/websites/websites.dto.ts
@@ -23,9 +23,9 @@ export class UpdateWebsiteDto {
   @IsString()
   @IsNotEmpty()
   @IsOptional()
-  @Matches(/^(?!-)[A-Za-z0-9-]{1,63}(?<!-)$/, {
+  @Matches(/^(?!-)[A-Za-z0-9-]{2,20}(?<!-)$/, {
     message:
-      'The name can be 1-63 characters long and contain letters, numbers, or hyphens (except at the beginning and end). Spaces and special characters are not allowed.',
+      'The name can be 2-20 characters long and contain letters, numbers, or hyphens (except at the beginning and end). Spaces and special characters are not allowed.',
   })
   name: string
 
