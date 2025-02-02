28e5b73ea4ab26f34f972897b41962fae7cc8415
Владислав <vladislav@MacBook-Air-Vladislav.local>
2025-02-02 21:43:58 +0300

    feat: added static serve of images

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,8 @@ import { AppService } from './app.service'
 import { WebsitesModule } from './modules/websites/websites.module'
 import { AuthModule } from './modules/auth/auth.module'
 import { FileModule } from './modules/file/file.module'
+import { ServeStaticModule } from '@nestjs/serve-static'
+import { join } from 'path'
 
 @Module({
   imports: [
@@ -13,6 +15,13 @@ import { FileModule } from './modules/file/file.module'
     MongooseModule.forRoot(
       `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.saufa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`,
     ),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '..', 'uploads'),
+      serveRoot: '/uploads/',
+      serveStaticOptions: {
+        extensions: ['jpg', 'jpeg', 'png', 'gif', 'svg', 'webp'],
+      },
+    }),
     WebsitesModule,
     AuthModule,
     FileModule,
